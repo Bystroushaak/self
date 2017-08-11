55cc269ab23ac751e6b42531433dce259fcceea8
Bystroushaak <bystrousak@kitakitsune.org>
2017-08-11 23:41:37 +0200

    Removed unused code from the previous experiments.

--- objects/glue/xlib_glue.cpp
+++ objects/glue/xlib_glue.cpp
@@ -484,55 +484,10 @@ void XDrawString16_wrap(Display *display, Drawable drawable,
     XDrawString16(display, drawable, gc, x, y, xstr, str->length());
 }
 
-
-int utf8toXChar2b(XChar2b *output_r, int outsize, const char *input, int inlen){
-	int j, k;
-	for(j =0, k=0; j < inlen && k < outsize; j ++){
-		unsigned char c = input[j];
-		if (c < 128)  {
-			output_r[k].byte1 = 0;
-			output_r[k].byte2 = c; 
-			k++;
-		} else if (c < 0xC0) {
-			/* we're inside a character we don't know  */
-			continue;
-		} else switch(c&0xF0){
-		case 0xC0: case 0xD0: /* two bytes 5+6 = 11 bits */
-			if (inlen < j+1){ return k; }
-			output_r[k].byte1 = (c&0x1C) >> 2;
-			j++;
-			output_r[k].byte2 = ((c&0x3) << 6) + (input[j]&0x3F);
-			k++;
-			break;
-		case 0xE0: /* three bytes 4+6+6 = 16 bits */ 
-			if (inlen < j+2){ return k; }
-			j++;
-			output_r[k].byte1 = ((c&0xF) << 4) + ((input[j]&0x3C) >> 2);
-			c = input[j];
-			j++;
-			output_r[k].byte2 = ((c&0x3) << 6) + (input[j]&0x3F);
-			k++;
-			break;
-		case 0xFF:
-			/* the character uses more than 16 bits */
-			continue;
-		}
-	}
-	return k;
-}
-
 XFontSet font_set = NULL;
 void Xutf8DrawString_wrap(Display *display, Drawable drawable, GC gc, int x,
                           int y, char *text, int length, void *FH){
   int len = strlen(text);
-  // puts(text);
-  // printf("Xutf8DrawString(%s)\n", text);
-
-  // XChar2b *string;
-  // string = (XChar2b *) malloc(sizeof(*string) * len);
-  // len = utf8toXChar2b(string, len, text, len);
-  // XDrawString16(display, drawable, gc, x, y, string, len);
-
   Xutf8DrawString(display, drawable, font_set, gc, x, y, text, len);
 }
 
